cf625cbc876ee3e07530fde22be215c16d771cea
patitojavi <patojavier1981@gmail.com>
2025-05-24 18:34:05 -0400

    avance

---
+++ backend/src/middlewares/role.middleware.ts
@@ -0,0 +1,20 @@
+import { Response, NextFunction } from 'express';
+import { AuthRequest } from './auth.middleware';
+
+export const verifyRole = (roles: ('admin' | 'capacitador')[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction): void => {
+    const user = req.user;
+
+    if (!user) {
+      res.status(401).json({ message: 'Token no válido o ausente' });
+      return;
+    }
+
+    if (!roles.includes(user.role)) {
+      res.status(403).json({ message: 'Acceso denegado: rol insuficiente' });
+      return;
+    }
+
+    next();
+  };
+};
