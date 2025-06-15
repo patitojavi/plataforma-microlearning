b132a464e797cc805a038c30a1d327567a012a88
patitojavi <patojavier1981@gmail.com>
2025-06-15 15:28:06 -0400

    backend/ se agregaron controllers para usuarios, obtener todos usuarios, obtener usuario por id, actualizar usuario, eliminar usuario

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import { verifyToken } from '../middlewares/auth.middleware';
 import { verifyRole } from '../middlewares/role.middleware';
+import {
+  obtenerUsuarios,
+  obtenerUsuario,
+  actualizarUsuario,
+  eliminarUsuario
+} from '../controllers/user.controller';
 
 const router = Router();
 
@@ -9,12 +15,16 @@ router.get('/admin', verifyToken, verifyRole(['admin']), (req, res) => {
   console.log('✅ Acceso correcto como ADMIN');
   res.json({ message: 'Bienvenido administrador' });
 });
+// Obtener todos los usuarios
+router.get('/', verifyToken, verifyRole(['admin']), obtenerUsuarios);
 
 // Ruta solo para capacitador
 router.get('/capacitador', verifyToken, verifyRole(['capacitador']), (req, res) => {
   console.log('✅ Acceso correcto como CAPACITADOR');
   res.json({ message: 'Bienvenido capacitador' });
 });
+// Obtener un usuario por ID
+router.get('/:id', verifyToken, verifyRole(['admin']), obtenerUsuario);
 
 // Ruta accesible por admin o capacitador
 router.get('/privado', verifyToken, verifyRole(['admin', 'capacitador']), (req, res) => {
@@ -22,11 +32,15 @@ router.get('/privado', verifyToken, verifyRole(['admin', 'capacitador']), (req, 
   console.log(`✅ Acceso correcto como ${rol?.toUpperCase()}`);
   res.json({ message: `Hola ${rol}, puedes ver esta ruta privada` });
 });
+// Actualizar un usuario
+router.put('/:id', verifyToken, verifyRole(['admin']), actualizarUsuario);
 
 // Ruta para cualquier usuario logueado
 router.get('/normal', verifyToken, (req, res) => {
   console.log('✅ Acceso como usuario logueado (cualquier rol)');
   res.json({ message: 'Bienvenido, usuario con token válido' });
 });
+// Eliminar un usuario
+router.delete('/:id', verifyToken, verifyRole(['admin']), eliminarUsuario);
 
 export default router;
