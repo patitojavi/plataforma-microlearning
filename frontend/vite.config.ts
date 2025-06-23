556a409020744aa81bf16de4da5d10c95b0bf998
nelson <nelson2005abner@gmail.com>
2025-06-23 00:23:05 -0400

    mas cositas

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,11 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+
+  build: {
+    outDir: 'dist',},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
